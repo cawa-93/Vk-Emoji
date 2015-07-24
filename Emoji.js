43075966822f73a14992ed43ac8fdff9bd74d26a
Alex Kozack <cawa-93@yandex.ru>
2015-07-24 10:16:38 +0300

    Add comments

--- Emoji.js
+++ Emoji.js
@@ -1,13 +1,38 @@
 if (!window.Emoji) {
+	/**
+	 * Class для смайликов
+	 * @class
+	 * @type {Object}
+	 */
 	var Emoji = {
+		/**
+		 * @type {RegExp}
+		 */
 		emojiCharSeq: /[0-9\uD83D\uD83C]/,
+		/**
+		 * @type {RegExp}
+		 */
 		emojiRegEx: /((?:[\uE000-\uF8FF\u270A-\u2764\u2122\u231B\u25C0\u25FB-\u25FE\u2615\u263a\u2648-\u2653\u2660-\u2668\u267B\u267F\u2693\u261d\u26A0-\u26FA\u2708\u2702\u2601\u260E]|[\u2600\u26C4\u26BE\u23F3\u2705\u2764]|[\uD83D\uD83C][\uDC00-\uDFFF]|\uD83C[\uDDE6-\uDDFF]\uD83C[\uDDE6-\uDDFF]|[0-9]\u20e3|[\u200C\u200D])+)/g,
+		/**
+		 * Path до директории с изображениями смайликов
+		 * @type {String}
+		 */
 		pathToEmojisImages: 'https://vk.com/images/emoji/',
 
+		/**
+		 * @param  {String} code   Код символа
+		 * @param  {String} symbol Сам символ
+		 * @return {String}        HTML код картинки
+		 */
 		getEmojiHTML: function (code, symbol) {
 			return '<img class="emoji" ' + (symbol ? 'alt="' + symbol + '"' : '') + ' src="' + this.pathToEmojisImages + code+'.png" />';
 		},
 
+		/**
+		 * Преобразует символы смайликов в строке в соотведствующие изображения
+		 * @param  {String} str Строка для преобразования
+		 * @return {String}     Изменённая строка
+		 */
 		emojiToHTML: function (str) {
 			str = str.replace(/&nbsp;/g, ' ').replace(/<br>/g, "\n");
 			var regs = {
@@ -64,6 +89,10 @@ if (!window.Emoji) {
 			return str;
 		},
 
+		/**
+		 * Подбирает коды к найденным символам
+		 * @param  {String} symbolstr Символ для замены
+		 */
 		emojiReplace: function (symbolstr) {
 			var i = 0;
 			var buffer = '', altBuffer = '', num;
